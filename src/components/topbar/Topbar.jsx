25249702613184a5e1114919a8f0faa10945c3c2
faticantimatteo97 <faticantimatteo97@gmail.com>
2021-05-23 17:50:49 +0200

    fix homepage

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,7 +9,7 @@ function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">Lamasocial</span>
+        <Link to="/" className="logo" >Socialcloud</Link>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
@@ -21,25 +21,9 @@ function Topbar() {
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarLinks">
-          <Link to="/" className="span spann">Homepage</Link>
-          <span className="topbarLink">Timeline</span>
-        </div>
-        <div className="topbarIcons">
-          <div className="topbarIconItem">
-            <Person />
-            <span className="topbarIconBadge">1</span>
-          </div>
-          <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">2</span>
-          </div>
-          <div className="topbarIconItem">
-            <Notifications />
-            <span className="topbarIconBadge">1</span>
-          </div>
-        </div>
-        <Link to="/profile" className="btn btn-primary">hello1</Link>
+        <Link to="/profile">
+        <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+        </Link>
       </div>
     </div>
   );
